320db52a3dd2907276f18ed36a311244a8632f84
Zhongrui Xu <zhongruixu@gmail.com>
2019-11-04 22:02:29 +0800

    init

---
+++ Algorithm/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Algorithm
+//
+//  Created by Zhongrui on 2019/11/4.
+//  Copyright © 2019 zrxu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
